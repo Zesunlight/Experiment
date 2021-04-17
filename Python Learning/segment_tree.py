39b31b5cae21de2043e16fd6fb1df655568bc291
赵永泽 <2714616793@qq.com>
2021-04-17 21:27:02 +0800

    seg tree

---
+++ Python Learning/segment_tree.py
@@ -0,0 +1,50 @@
+# -*- coding: UTF-8 -*-
+"""=================================================
+@Project: how_to_do
+@File   : segment_tree
+@IDE    : PyCharm
+@Author : ZYZ
+@Date   : 21/4/17
+@Des    : in vain
+=================================================="""
+from math import log2, pow, ceil
+
+
+class SegTree:
+    # https://oi-wiki.org/ds/seg/
+
+    def __init__(self, data, tree):
+        self.data = data
+        self.tree = tree
+        self.num = len(data)
+
+    def build_sum(self, start, end, pos=1):
+        if start == end:
+            self.tree[pos] = self.data[start]
+        else:
+            mid = (start + end) // 2
+            self.build_sum(start, mid, 2*pos)
+            self.build_sum(mid+1, end, 2*pos+1)
+            self.tree[pos] = self.tree[2*pos] + self.tree[2*pos+1]
+
+    def get_sum(self, start, end, left, right, pos):
+        # [l, r]为查询区间, [s, e]为当前节点包含的区间, p为当前节点的编号
+        if (left <= start) and (end <= right):
+            return tree[pos]
+
+        mid = (start + end) // 2
+        s = 0
+        if left <= mid:
+            s += self.get_sum(start, mid, left, right, 2*pos)
+        if right > mid:
+            s += self.get_sum(mid+1, end, left, right, 2*pos+1)
+        return s
+
+
+if __name__ == '__main__':
+    data = list(range(10))
+    tree = [-1] * int(pow(2, ceil(log2(len(data))) + 1))
+    st = SegTree(data, tree)
+    st.build_sum(0, len(data) - 1)
+    print(st.tree)
+    print(st.get_sum(0, len(data)-1, 1, 3, 1))
